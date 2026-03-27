01b1d5f5e10e671aff40be20d4e51383377c6d37
Syed Saaqibuddin <syedsaaqibuddin@gmail.com>
2026-03-27 18:14:33 +0530

    list program1
    list program that calculates maximum, minimum,sum

---
+++ list1.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+#input n prices find the highest price, lowest price, sum of price
+
+prices=eval(input("Enter all price  s:"))
+
+print("max price :",max(prices))
+print("Min Price :",min(prices))
+print("Sum Price :",sum(prices))
